18aa7db06311becdcea5d3303a596b62c301d309
ben <benedict.ongys@gmail.com>
2025-07-05 17:50:12 +0800

    Added framerate to reduce resource use

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     switch = True
     while switch:
         for event in pygame.event.get():
@@ -12,6 +14,8 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        delta = clock.tick(60)
+        dt = delta / 1000
 
 
 if __name__ == "__main__":
